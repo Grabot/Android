df89f2353af6069f3dc80654052d310f47ae949a
Grabot <SanderKools@gmail.com>
2017-12-15 17:30:31 +0100

    tiny refactor
    just a very basic part of the refactoring

--- cityBuilder/core/src/cityBuilder/load/TilesLoad.java
+++ cityBuilder/core/src/cityBuilder/load/TilesLoad.java
@@ -6,21 +6,17 @@ import cityBuilder.objects.Tile;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
-public class TilesLoad extends Data
-{
+public class TilesLoad extends Data {
 	private Tile[][] tile = new Tile[gridSizeWidth][gridSizeHeight];
 
 	public ArrayList<Tile> tiles = new ArrayList<Tile>();
 	public ArrayList<ArrayList<Tile>> tiles2D = new ArrayList<ArrayList<Tile>>();
 
-	public TilesLoad(TextureAtlas atlas)
-	{
+	public TilesLoad(TextureAtlas atlas) {
 
-		for( int x = 0; x < gridSizeWidth; x++ )
-		{
+		for( int x = 0; x < gridSizeWidth; x++ ) {
 			tiles2D.add(new ArrayList<Tile>());
-			for( int y = 0; y < gridSizeHeight; y++ )
-			{
+			for( int y = 0; y < gridSizeHeight; y++ ) {
 				tile[x][y] = new Tile( new Vector((-tileStartWidth + (tileWidth * y)), (tileStartHeight - (tileHeight * x))), atlas);
 				tile[x][y].setName("(" + x + "," + y + ")");
 				tiles2D.get(x).add(tile[x][y]);
@@ -30,19 +26,14 @@ public class TilesLoad extends Data
 		setAttributes attributes = new setAttributes(tile);
 		attributes.attributes();
 
-		for( int x = 0; x < gridSizeWidth; x++ )
-		{
-			for( int y = 0; y < gridSizeHeight; y++ )
-			{
-				System.out.print("; " + tiles2D.get(x).get(y).getName());
+		for( int x = 0; x < gridSizeWidth; x++ ) {
+			for( int y = 0; y < gridSizeHeight; y++ ) {
 				tiles.add( attributes.getTile(x, y) );
 			}
-			System.out.println("");
 		}
 	}
 
-	public ArrayList<Tile> getTiles()
-	{
+	public ArrayList<Tile> getTiles() {
 		return tiles;
 	}
 }
