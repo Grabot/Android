128a2fbd1def9e1e3d430a24f0a86ae4a797decc
Grabot <SanderKools@gmail.com>
2018-03-31 18:19:05 +0200

    road texture fix
    Added more texture options, this way the rotation would not be an
    option.

--- cityBuilder/core/src/cityBuilder/gameScreen/buildings/Road.java
+++ cityBuilder/core/src/cityBuilder/gameScreen/buildings/Road.java
@@ -15,6 +15,7 @@ public class Road implements Building {
     private Tile[] adjacentRoads;
     private boolean[] adjacent;
     private Tile roadTile;
+    private TextureRegion[] roads;
     private TextureRegion roadNo;
     private TextureRegion roadSingle;
     private TextureRegion roadDouble;
@@ -31,44 +32,62 @@ public class Road implements Building {
         roadTriple = atlas.findRegion("roadTriple");
         roadDoubleForward = atlas.findRegion("roadDoubleForward");
         roadQuadruppel = atlas.findRegion("roadQuadruppel");
+        roads = new TextureRegion[16];
+        roads[0] = atlas.findRegion("roadNo");
+        roads[1] = atlas.findRegion("roadSingle1");
+        roads[2] = atlas.findRegion("roadSingle2");
+        roads[3] = atlas.findRegion("roadSingle3");
+        roads[4] = atlas.findRegion("roadSingle4");
+        roads[5] = atlas.findRegion("roadDouble1");
+        roads[6] = atlas.findRegion("roadDouble2");
+        roads[7] = atlas.findRegion("roadDouble3");
+        roads[8] = atlas.findRegion("roadDouble4");
+        roads[9] = atlas.findRegion("roadDoubleForward1");
+        roads[10] = atlas.findRegion("roadDoubleForward2");
+        roads[11] = atlas.findRegion("roadTriple1");
+        roads[12] = atlas.findRegion("roadTriple2");
+        roads[13] = atlas.findRegion("roadTriple3");
+        roads[14] = atlas.findRegion("roadTriple4");
+        roads[15] = atlas.findRegion("roadQuadruppel");
         adjacentRoads = new Tile[4];
         adjacent = new boolean[4];
     }
 
     @Override
     public void render(Batch batch, int buildingPosition, float x, float y, int globalRotation) {
-        if (adjacent[0] && adjacent[1] && adjacent[2] && adjacent[3]) {
-            batch.draw( roadQuadruppel, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
-        } else if (!adjacent[0] && adjacent[1] && adjacent[2] && adjacent[3]) {
-            batch.draw( roadTriple, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 270, false);
-        } else if (adjacent[0] && !adjacent[1] && adjacent[2] && adjacent[3]) {
-            batch.draw( roadTriple, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
-        } else if (adjacent[0] && adjacent[1] && !adjacent[2] && adjacent[3]) {
-            batch.draw( roadTriple, x - 45, y - 23, 32, 32, 90, 48, 1, 1,90, false);
-        } else if (adjacent[0] && adjacent[1] && adjacent[2] && !adjacent[3]) {
-            batch.draw( roadTriple, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 180, false);
-        } else if (!adjacent[0] && !adjacent[1] && adjacent[2] && adjacent[3]) {
-            batch.draw( roadDouble, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 270, false);
-        } else if (adjacent[0] && !adjacent[1] && !adjacent[2] && adjacent[3]) {
-            batch.draw( roadDouble, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
-        } else if (adjacent[0] && adjacent[1] && !adjacent[2] && !adjacent[3]) {
-            batch.draw( roadDouble, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 90, false);
-        } else if (!adjacent[0] && adjacent[1] && adjacent[2] && !adjacent[3]) {
-            batch.draw( roadDouble, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 180, false);
-        } else if (!adjacent[0] && adjacent[1] && !adjacent[2] && adjacent[3]) {
-            batch.draw( roadDoubleForward, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 90, false);
-        } else if (adjacent[0] && !adjacent[1] && adjacent[2] && !adjacent[3]) {
-            batch.draw( roadDoubleForward, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        if (!adjacent[0] && !adjacent[1] && !adjacent[2] && !adjacent[3]) {
+            batch.draw(roads[0], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
         } else if (adjacent[0] && !adjacent[1] && !adjacent[2] && !adjacent[3]) {
-            batch.draw( roadSingle, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+            // if there is only 1 road tile to the right below of this one.
+            batch.draw(roads[1], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
         } else if (!adjacent[0] && adjacent[1] && !adjacent[2] && !adjacent[3]) {
-            batch.draw( roadSingle, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 90, false);
+            batch.draw(roads[2], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
         } else if (!adjacent[0] && !adjacent[1] && adjacent[2] && !adjacent[3]) {
-            batch.draw( roadSingle, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 180, false);
+            batch.draw(roads[3], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
         } else if (!adjacent[0] && !adjacent[1] && !adjacent[2] && adjacent[3]) {
-            batch.draw( roadSingle, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 270, false);
-        } else if (!adjacent[0] && !adjacent[1] && !adjacent[2] && !adjacent[3]) {
-            batch.draw(roadNo, x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+            batch.draw(roads[4], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (adjacent[0] && adjacent[1] && !adjacent[2] && !adjacent[3]) {
+            batch.draw(roads[5], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (!adjacent[0] && adjacent[1] && adjacent[2] && !adjacent[3]) {
+            batch.draw(roads[6], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (!adjacent[0] && !adjacent[1] && adjacent[2] && adjacent[3]) {
+            batch.draw(roads[7], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (adjacent[0] && !adjacent[1] && !adjacent[2] && adjacent[3]) {
+            batch.draw(roads[8], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (adjacent[0] && !adjacent[1] && adjacent[2] && !adjacent[3]) {
+            batch.draw(roads[9], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (!adjacent[0] && adjacent[1] && !adjacent[2] && adjacent[3]) {
+            batch.draw(roads[10], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (adjacent[0] && adjacent[1] && adjacent[2] && !adjacent[3]) {
+            batch.draw(roads[11], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (!adjacent[0] && adjacent[1] && adjacent[2] && adjacent[3]) {
+            batch.draw(roads[12], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (adjacent[0] && !adjacent[1] && adjacent[2] && adjacent[3]) {
+            batch.draw(roads[13], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (adjacent[0] && adjacent[1] && !adjacent[2] && adjacent[3]) {
+            batch.draw(roads[14], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
+        } else if (adjacent[0] && adjacent[1] && adjacent[2] && adjacent[3]) {
+            batch.draw(roads[15], x - 45, y - 23, 32, 32, 90, 48, 1, 1, 0, false);
         }
     }
 
